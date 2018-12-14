32ac40204399a27e493008e340416a1f6effc2e4
tpapka <36523428+tpapka@users.noreply.github.com>
2018-12-14 12:01:07 -0700

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,18 @@
+import requests
+
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+
+r = requests.get(url)
+
+submission_ids = r.json()
+submission_dicts = []
+
+for sid in submission_ids[:30]:
+    url = ('https://hacker-news.firebaseio.com/v0/item/' + str(sid) + '.json')
+    submission_r = requests.get(url)
+    response_dict = submission_r.json()
+    submission_dict = {'title': response_dict['title'], 'link': response_dict['url']}
+    submission_dicts.append(submission_dict)
+
+for sd in submission_dicts:
+    print(sd['title'])
